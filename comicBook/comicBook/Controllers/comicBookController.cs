9f55d58e207def4271732e2e6c9be6196063a32d
uddiefia <udaradamitha@gmail.com>
2017-10-14 13:08:58 +0530

    creating controller

---
+++ comicBook/comicBook/Controllers/comicBookController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBook.Controllers
+{
+    public class comicBookController : controller
+    {
+    }
+}
